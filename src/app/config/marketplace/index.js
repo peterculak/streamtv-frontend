67f5d994a38105ae65b139763c9aefd7b08343e8
Peter Culak <peter.culak@worldfirst.com>
2018-12-04 22:12:13 +0000

    Adding dotenv

--- src/app/config/marketplace/index.js
+++ src/app/config/marketplace/index.js
@@ -1,7 +1,5 @@
-// const scheme = process.env.MKPID_API_SCHEME;
-// const host = process.env.MKPID_API_HOST;
-const scheme = 'http';
-const host = '192.168.99.95';
+const scheme = process.env.REACT_APP_MKPID_API_SCHEME;
+const host = process.env.REACT_APP_MKPID_API_HOST;
 
 const config = {
   api: {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import ReactAppKernel from './Kernel';
+import dotenv from 'dotenv';
+dotenv.load();
 
 const element = document.getElementById('mkpid-app');
 if (element) {
