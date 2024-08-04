47397f5f6b4bebbbc7c2b0863fa56aa12ac8183a
Peter Culak <peter.culak@outlook.com>
2024-08-04 18:36:55 +0100

    Updates

---
+++ precache-manifest.a607260346dcfec4ba6632ba5dae1a82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fb396a18a858f3c5f7b",
+    "url": "https://peterculak.github.io/streamtv-frontend/static/css/main.5810a0d4.chunk.css"
+  },
+  {
+    "revision": "3fb396a18a858f3c5f7b",
+    "url": "https://peterculak.github.io/streamtv-frontend/static/js/main.3fb396a1.chunk.js"
+  },
+  {
+    "revision": "66631866713ee273db2b",
+    "url": "https://peterculak.github.io/streamtv-frontend/static/js/1.66631866.chunk.js"
+  },
+  {
+    "revision": "d299c8864b49d6060cb4",
+    "url": "https://peterculak.github.io/streamtv-frontend/static/js/runtime~main.d299c886.js"
+  },
+  {
+    "revision": "1362baf40cb95ca3c2eba709cf5c44af",
+    "url": "https://peterculak.github.io/streamtv-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8744341e966ba45f91593bf841c9cb7.js"
+  "https://peterculak.github.io/streamtv-frontend/precache-manifest.a607260346dcfec4ba6632ba5dae1a82.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://peterculak.github.io/streamtv-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
