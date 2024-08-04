1b785b6cc681f177e3e71b08f3ccaac94e4b1229
Peter Culak <peter.culak@outlook.com>
2024-08-04 18:16:28 +0100

    Updates

--- precache-manifest.da657b9b02d98140ea1a267527520870.js
+++ precache-manifest.da657b9b02d98140ea1a267527520870.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "284b3457870858c7ead3",
-    "url": "/static/css/main.5810a0d4.chunk.css"
+    "url": "/streamtv-frontend/static/css/main.5810a0d4.chunk.css"
   },
   {
     "revision": "284b3457870858c7ead3",
-    "url": "/static/js/main.284b3457.chunk.js"
+    "url": "/streamtv-frontend/static/js/main.284b3457.chunk.js"
   },
   {
     "revision": "5ece38983fdec4366ad4",
-    "url": "/static/js/1.5ece3898.chunk.js"
+    "url": "/streamtv-frontend/static/js/1.5ece3898.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "/streamtv-frontend/static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "15d3e1b3701a33ffdc1b9bae2736049b",
-    "url": "/index.html"
+    "url": "/streamtv-frontend/index.html"
   }
 ];
